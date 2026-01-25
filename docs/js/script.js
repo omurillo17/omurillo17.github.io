0c2c2bb16cd83ea2484f1b8a6a6df114d97cfd4c
Geronimo Murillo <oscarone2002@gmail.com>
2026-01-24 19:54:34 -0600

    Nuevos cambios, 24/01/2026

--- docs/js/script.js
+++ docs/js/script.js
@@ -9,3 +9,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
     console.log("Portafolio cargado y listo 🚀");
 }); 
+
+function copiarCorreo(event) {
+    event.preventDefault();
+    
+    const correo = "oscxrma@gmail.com";
+    const boton = document.getElementById("btn-email");
+    const textoOriginal = boton.innerHTML;
+
+    navigator.clipboard.writeText(correo).then(() => {
+        boton.innerHTML = "¡Copiado!";
+        boton.style.backgroundColor = "#22c55e";
+
+        setTimeout(() => {
+            boton.innerHTML = textoOriginal;
+            boton.style.backgroundColor = "";
+            booton.innerHTML = "Envíame un correo ";
+        }, 2000);
+    }).catch(err => {
+        console.error('Error al copiar: ', err);
+    });
+}
